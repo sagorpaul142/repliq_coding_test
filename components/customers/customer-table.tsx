2113eee134d282d9020bbd23c0d196f145a19cd7
sagorpaul142 <sagorpaulcse@gmail.com>
2024-04-07 19:13:58 +0600

    build error solved

--- components/customers/customer-table.tsx
+++ components/customers/customer-table.tsx
@@ -158,7 +158,7 @@ const CustomerTable = () => {
               <SheetHeader>
                 <SheetTitle>Edit User</SheetTitle>
                 <SheetDescription>
-                  Make changes to your profile here. Click save when you're done.
+                  Make changes to your profile here. Click save when you&apos;re done.
                 </SheetDescription>
               </SheetHeader>
               <Form {...formStep1}>
