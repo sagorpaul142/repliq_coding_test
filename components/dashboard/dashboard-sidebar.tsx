c8083ec25d9a17d9529a814fd0fc0cd8c465449a
sagorpaul142 <sagorpaulcse@gmail.com>
2024-04-07 18:17:31 +0600

    Logo add

--- components/dashboard/dashboard-sidebar.tsx
+++ components/dashboard/dashboard-sidebar.tsx
@@ -111,11 +111,7 @@ const DashboardSidebar = ({children}: { children: ReactNode }) => {
         {/* Sidebar component, swap this element with another sidebar if you like */}
         <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
           <div className="flex h-16 shrink-0 items-center">
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
+            <Icons.logo />
           </div>
           <nav className="flex flex-1 flex-col">
             <ul role="list" className="flex flex-1 flex-col gap-y-7">
